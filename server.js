56c7e853e890417386524a21ec641929dd6347ce
Tess <tess.thornley@gmail.com>
2019-03-30 19:44:27 +0000

    mailgun feature on poll submit added

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const vO = require('./knex_view_options');
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
 const pollRoutes = require("./routes/polls");
+const mailgun = require('./test-mailgun.js')
 
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
@@ -47,20 +48,38 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-function create_poll() {
+// function create_poll() {
 
-}
+// }
 
 // (3) app.get to /submitted after submission
-app.post("/poll_submitted", function(req, res) {
-  let admin_link = random_url_gen();
-  let participant_link = random_url_gen();
+
+app.post("/poll_submitted", function(req, res) {  
+    let admin_link = random_url_gen();
+    let participant_link = random_url_gen();
+
+    mailgun.sendEmail(req.body.email, admin_link, participant_link);
+  });
+  // function renderLinks(callback) {
+  //   let admin_link = random_url_gen();
+  //   let participant_link = random_url_gen();
+  //   console.log(admin_link)
+  //   callback(req.body.email, admin_link, participant_link);
+  // };
+
+  // renderLinks(function(email, admin_link, participant_link) {
+  //   let mailgun = require('./test-mailgun.js')
+  //   mailgun.sendEmail(email, admin_link, participant_link);
+  //   res.redirect('poll_submitted', {admin_link:admin_link});
+  // })
+
   // let templateVars = {admin_link: admin_link,
   //                     participant_link: participant_link,
   //                     poll_question: req.body.question,
   //                     creator_email: req.body.email,
   //                     title: req.body.title,
   //                     description: req.body.description};
+
   // create_poll(templateVars);
 
   // console.log(templateVars);
@@ -70,16 +89,11 @@ app.post("/poll_submitted", function(req, res) {
 
   // res.render(SUCCESSFUL SUBMISSION PAGE (w/ links to both admin, participation url))
 
-<<<<<<< HEAD
 // //   }
 // //   res.render("take_poll");
 // //   //check if userURL exists in database
 // //   res.render("take_poll");
-// // });
 
-app.post("/u/:userURL", function(req, res) {
-  res.redirect("thanks")
-});
 
 app.get("/a/:adminURL", function(req, res) {
   //check if adminURL exists in database
--- test-mailgun.js
+++ test-mailgun.js
@@ -1,16 +1,20 @@
-const mailgun = require("mailgun-js");
-const api_key = '1b55f1ba840b0e3ba742b39f6811bfe1-e51d0a44-705723d4';
-const DOMAIN = 'sandbox9f50a03bac7f4d0ea70fd0fe5095b553.mailgun.org';
-const mg = mailgun({apiKey: api_key, domain: DOMAIN});
+function sendEmail(userEmail, admin_link, participant_link) {  
+  const mailgun = require("mailgun-js");
+  const api_key = '8f99504a1307b48bd3e0ab1a2e8444e8-e51d0a44-f0897a36';
+  const DOMAIN = 'sandboxb9cce0deafc74d0ca9548d503a524d1e.mailgun.org';
+  const mg = mailgun({apiKey: api_key, domain: DOMAIN});
 
-const data = {
-  from: 'mailgun@sandbox9f50a03bac7f4d0ea70fd0fe5095b553.mailgun.org',
-  to: 'bleung9@gmail.com',
-  subject: 'Hello',
-  text: 'Testing some Mailgun awesomness!'
-};
+  const data = {
+    from: 'mailgun@sandboxb9cce0deafc74d0ca9548d503a524d1e.mailgun.org',
+    to: userEmail,
+    subject: 'Your Poll Links',
+    text: `Admin link: <a href="localhost:8080/a/${admin_link}"></a>. Participant link: <a href="localhost:8080/u/${participant_link}></a>.` 
+  };
 
-mg.messages().send(data, function (error, body) {
-  console.log(error);
-  console.log(body);
-});
+  mg.messages().send(data, function (error, body) {
+    console.log(error);
+    console.log(body);
+  });
+}
+
+module.exports = {sendEmail:sendEmail}
