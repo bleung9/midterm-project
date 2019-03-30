ee04258417c3ff47a9ef38d7e94aabb6b3814773
Tess <tess.thornley@gmail.com>
2019-03-30 21:27:18 +0000

    mailgun api key updated

--- test-mailgun.js
+++ test-mailgun.js
@@ -1,11 +1,11 @@
 function sendEmail(userEmail, admin_link, participant_link) {  
   const mailgun = require("mailgun-js");
-  const api_key = '8f99504a1307b48bd3e0ab1a2e8444e8-e51d0a44-f0897a36';
-  const DOMAIN = 'sandboxb9cce0deafc74d0ca9548d503a524d1e.mailgun.org';
+  const api_key = 'a1bf973d5fa8a7a5d63cb26ac216299-e51d0a44-9058a772';
+  const DOMAIN = 'sandbox8e7c2926a76f4bbab06525d76094051f.mailgun.org';
   const mg = mailgun({apiKey: api_key, domain: DOMAIN});
 
   const data = {
-    from: 'mailgun@sandboxb9cce0deafc74d0ca9548d503a524d1e.mailgun.org',
+    from: 'mailgun@sandbox8e7c2926a76f4bbab06525d76094051f.mailgun.org',
     to: userEmail,
     subject: 'Your Poll Links',
     text: `Admin link: <a href="localhost:8080/a/${admin_link}"></a>. Participant link: <a href="localhost:8080/u/${participant_link}></a>.` 
